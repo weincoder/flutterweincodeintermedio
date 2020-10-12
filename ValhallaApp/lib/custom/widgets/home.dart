1713e784b01456f065e5358917252e5175915f61
Danielherresan <danielherresan@gmail.com>
2020-10-12 15:48:17 -0500

    subiendo el stream builder

--- ValhallaApp/lib/custom/widgets/home.dart
+++ ValhallaApp/lib/custom/widgets/home.dart
@@ -1,21 +1,20 @@
+import 'package:ValhallaApp/custom/widgets/fundation/data.dart';
 import 'package:ValhallaApp/custom/widgets/fundation/ourText.dart';
 import 'package:ValhallaApp/custom/widgets/our_app_bar.dart';
 import 'package:ValhallaApp/custom/widgets/our_footer.dart';
 import 'package:ValhallaApp/custom/widgets/our_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 
 import 'package:flutter/services.dart';
 class Home extends StatelessWidget {
 
-
-  Future <String> _getPath () async {
-    await Future.delayed(Duration (seconds: 5));
-    return 'assets/images/space.jpg';
-  }
-  Future<String> loadFileAsync() async {
-    await Future.delayed(Duration (seconds: 2));
-    return   rootBundle.loadString('assets/datos.txt');
+  Stream <String>  _getStreamPath() async*{
+    for (var item in jsonData){
+      await Future.delayed(Duration (seconds: 2));
+      yield item['pathImage'];
+    }
   }
 
   @override
@@ -26,30 +25,34 @@ class Home extends StatelessWidget {
             child: Column(
               children: [
                 HeadingImage(text: 'Inmediatamente',),
-                FutureBuilder(
-                    future: _getPath(),
-                    builder: (contex, AsyncSnapshot snaphot){
-                      if (snaphot.hasError){
-                        return H2 (text: 'Error : ${snaphot.error.toString()}');
-                      }
-                      if (!snaphot.hasData){
-                        return CircularProgressIndicator();
-                      }
-                      return OurImage(pathImage: snaphot.data, widthImage: 250, heightImage: 250);
+                StreamBuilder(
+                stream: _getStreamPath(),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  dynamic image = Container();
+                  if (snapshot.hasError){
+                      image = Text ('Error : ${snapshot.error.toString()}');
+                  }
+                  switch (snapshot.connectionState){
+                    case ConnectionState.none :
+                      image = Text ('None');
+                      break;
+                    case ConnectionState.waiting :
+                      image = CircularProgressIndicator();
+                      break;
+                    case ConnectionState.active :
+                      image = OurImage(pathImage: snapshot.data,
+                      heightImage: 300,
+                        widthImage: 300,
+                      );
+                      break;
+                    case ConnectionState.done :
+                      image =Center (child :
+                          HeadingImage(text: 'Todo ya se ha mostrado',));
+                  }
+                  return image;
+                },
 
-                    }),
-                FutureBuilder(
-                    future: loadFileAsync(),
-                    builder: (contex, AsyncSnapshot snaphot){
-                      if (snaphot.hasError){
-                        return H2 (text: 'Error : ${snaphot.error.toString()}');
-                      }
-                      if (!snaphot.hasData){
-                        return CircularProgressIndicator();
-                      }
-                      return H2 (text: snaphot.data,);
-
-                    }),
+                )
               ],
             )
           ),
